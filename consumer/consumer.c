d64770f49c407f78499ba6fd273bfe8e85902101
Yiran Wang <yiranw@wifi-131-179-25-58.host.ucla.edu>
2017-04-26 14:35:27 -0700

    Consumer and prducer with file sharing

--- consumer/consumer.c
+++ consumer/consumer.c
@@ -15,92 +15,12 @@
 #define BUFF_SIZE 1024
 
 
-int main(int argc, char **argv)
-{
-	/* Generic variables */
-	int i, idx;
-	int rv;
-	
-	/*input file name*/
-	char* ifile_name; // data file
-	
-	/*output file names*/
-	char* ofile_pt_name; // peak&trough file
-	char* ofile_st_name; // stride detection file
-	char* ofile_maxmin_name; // feature maxmin file(t, max, t, min)
-	char* ofile_feature_name; // feature file(max min period)
-
-	
-	FILE *fp;
-	char *line = NULL;
+int count_samples(FILE *fp) {
+	char * line = NULL;
 	size_t len = 0;
 	ssize_t read;
-	int N_SAMPLES;
+	int N_SAMPLES = 0;
 
-	// Original Data Containers
-	double* time;
-	double* accel_x;
-	double* accel_y;
-	double* accel_z;
-	double* gyro_x;
-	double* gyro_y;
-	double* gyro_z;
-
-	float pk_threshold;
-
-	double t1, t2; // variable used to parse time_before and time_after
-	double start_time; // variable used to computer sampling time
-       	/* Variables for peak-trough detection */	
-	int *P_i; 	// indicies of each peak found by peak detection
-	int *T_i; 	// indicies of each trough found by trough detection
-	int *S_i; 	// indicies of the start of each stride
-
-	int n_P; 	// number of peaks
-	int n_T; 	// number of troughs
-	int n_S; 	// number of strides
-
-	double* temp;
-	double mean_val;
-
-	/*
-	 * set this to 0 so that the code can function without
-	 * having to actually performing stride detection
-	 * from peaks and troughs
-	 */
-	n_S = 0; 
-	
-	/*
-	 * Check if the user entered the correct command line arguments
-	 * Usage: 
-	 * ./extract_stride_data <ifile_name> <output_peaks> <output_strides> <output maxmin file>
-	 * 				<output feature file> <threshold_value_float> <time constraint>
-	 * 
-	 */
-	if (argc != 7) {
-		fprintf(stderr, "USEAGE: ./stride_detector <input data file> <output peak trough detection file> <output stride detection file> <output maxmin file> <output feature file> <threshold>\n");
-		exit(EXIT_FAILURE);
-
-	} else {
-		ifile_name = argv[1];
-		ofile_pt_name = argv[2];
-		ofile_st_name = argv[3];
-		
-		ofile_maxmin_name = argv[4];
-		ofile_feature_name = argv[5];
-
-		pk_threshold = atof(argv[6]);
-	}
-
-	printf("Arguments used:\n\t%s=%s\n\t%s=%s\n\t%s=%s\n\t%s=%f\n",
-			"ifile_name", ifile_name,
-			"ofile_peak_trough_name", ofile_pt_name,
-			"ofile_stride_name", ofile_st_name,
-			"peak_threshold", pk_threshold
-	      );
-
-	/* open the input file */
-	printf("Attempting to read from file \'%s\'.\n", ifile_name);
-	fp = fopen(ifile_name, "r");
 	if (fp == NULL) {
 		fprintf(stderr, 
 				"Failed to read from file \'%s\'.\n", 
@@ -109,30 +29,32 @@ int main(int argc, char **argv)
 		sleep(15);
 		exit(EXIT_FAILURE);
 	}
-
 	/* count the number of lines in the file */
 	read = getline(&line, &len, fp); //discard header of file
 	N_SAMPLES = 0;
 	while ((read = getline(&line, &len, fp)) != -1) {
 		N_SAMPLES++;
 	}
-
 	/* go back to the start of the file so that the data can be read */
 	rewind(fp);
+	return N_SAMPLES;
+}
+
+int collect_raw_data(FILE *fp, double &time,
+					double* accel_x,
+					double* accel_y,
+					double* accel_z,
+					double* gyro_x,
+					double* gyro_y,
+					double* gyro_z,
+					double* t1, 
+					double* t2,
+					double* start_time) {
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
 	read = getline(&line, &len, fp); //discard header of file
-
-	/* start reading the data from the file into the data structures */
-	i = 0;
-
-	// allocate memory
-	time = (double*) malloc(sizeof(double) * N_SAMPLES);
-	accel_x = (double*) malloc(sizeof(double) * N_SAMPLES);
-	accel_y = (double*) malloc(sizeof(double) * N_SAMPLES);
-	accel_z = (double*) malloc(sizeof(double) * N_SAMPLES);
-	gyro_x = (double*) malloc(sizeof(double) * N_SAMPLES);
-	gyro_y = (double*) malloc(sizeof(double) * N_SAMPLES);
-	gyro_z = (double*) malloc(sizeof(double) * N_SAMPLES);
-
 	while ((read = getline(&line, &len, fp)) != -1) {
 		/* parse the data */
 		rv = sscanf(line, "%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n", &t1, &t2, &accel_x[i], &accel_y[i], &accel_z[i], &gyro_x[i], &gyro_y[i], &gyro_z[i]);
@@ -152,17 +74,79 @@ int main(int argc, char **argv)
 		time[i] = (t1 + t2)/2.0 - start_time;
 		i++;
 	}
-	fclose(fp);
+}
 
-	/* 
-	 * From selected thresholds, 
-	 * find indicies of peaks
-	 * find indicies of troughs
-	 */
+
+
+int process_file(const char *fname, float pk_threshold, const char *ofile_feature_name) {
+	//Generic variables
+	int i = 0;
+	int rv;
+
+	//Variables for file
+	char delete_file[1024];
+	FILE *fp;
+	int fd;
+	int N_SAMPLES;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	memset(delete_file, 0, 1024);
+
+	//Original Data Containers
+	double* time;
+	double* accel_x;
+	double* accel_y;
+	double* accel_z;
+	double* gyro_x;
+	double* gyro_y;
+	double* gyro_z;
+	double t1, t2; // variable used to parse time_before and time_after
+	double start_time; // variable used to computer sampling time
+
+	//Peak and trough variables
+	int *P_i; 	// indicies of each peak found by peak detection
+	int *T_i; 	// indicies of each trough found by trough detection
+	int *S_i; 	// indicies of the start of each stride
+	double *temp;
+	double mean_val;
+	int n_P; 	// number of peaks
+	int n_T; 	// number of troughs
+	int n_S = 0; 	// number of strides
+
+	// open the file to inspect
+	printf("\tOpening file \'%s\'\n", fname);
+	fp = fopen(fname, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Failed to open file \'%s\'. Exiting.\n", fname);
+		exit(EXIT_FAILURE);
+	}
+
+	// acquire the lock for the file (incase the producer is still writing to it)
+	fd = fileno(fp);
+	flock(fd, LOCK_EX);
+
+	//Collect sample number
+	N_SAMPLES = count_samples(fp);
+
+	//Allocate original data containers
+	time = (double*) malloc(sizeof(double) * N_SAMPLES);
+	accel_x = (double*) malloc(sizeof(double) * N_SAMPLES);
+	accel_y = (double*) malloc(sizeof(double) * N_SAMPLES);
+	accel_z = (double*) malloc(sizeof(double) * N_SAMPLES);
+	gyro_x = (double*) malloc(sizeof(double) * N_SAMPLES);
+	gyro_y = (double*) malloc(sizeof(double) * N_SAMPLES);
+	gyro_z = (double*) malloc(sizeof(double) * N_SAMPLES);
 	P_i = (int *) malloc(sizeof(int) * N_SAMPLES);
 	T_i = (int *) malloc(sizeof(int) * N_SAMPLES);
 	
+	
+	// Collect raw data
+	printf("\tAcquired lock. Printing file contents of \'%s\':\n", fname);
+	collect_raw_data(fp,time,accel_x,accel_y,accel_z,gyro_x,gyro_y,
+		gyro_z,t1,t2,start_time);
 
+	//Find peak and trough
 	rv = find_peaks_and_troughs(
 			gyro_z, 
 			N_SAMPLES, 
@@ -174,89 +158,80 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
-
-	// Filter troughs to filter out inappropriate values
+	//Filter troughs to filter out inappropriate values
 	printf("Attempting to filter troughs.\n");
 	S_i = (int *) malloc(sizeof(int) * n_T);
 	temp = (double *)malloc(sizeof(double) * n_T);
-
-	// Load all trough values into temp
 	for(i = 0; i < n_T; i++){
 		temp[i] = gyro_z[T_i[i]];
 	}
-
 	mean(temp, 0, n_T, &mean_val);
-
 	for(i = 0; i < n_T; i++){
 		if(gyro_z[T_i[i]] < mean_val){
 			S_i[n_S] = T_i[i];
 			n_S++;
 		}
 	}
+	
 
-	/* open the output file to write the peak and trough data */
-	printf("Attempting to write to file \'%s\'.\n", ofile_pt_name);
-	fp = fopen(ofile_pt_name, "w");
-	if (fp == NULL) {
-		fprintf(stderr, 
-				"Failed to write to file \'%s\'.\n", 
-				ofile_pt_name
-		       );
-		exit(EXIT_FAILURE);
-	}
+	//Collect global features
+	global_feature(accel_y,gyro_y,time,S_i,n_S,ofile_feature_name);
+	//Execute neural network
+	exe_global_neural_network(ofile_feature_name);
 
-	fprintf(fp, "P_t,P_x,T_t,T_p\n");
-	for (i = 0; i < n_P || i < n_T; i++) {
-		/* Only peak data if there is peak data to write */
-		if (i < n_P) {
-			idx = P_i[i];
-			fprintf(fp, "%20.10lf,%lf,",
-					time[idx],
-				gyro_z[idx]);
-		} else {
-			fprintf(fp, ",,,");
-		}
-		/* Only trough data if there is trough data to write */
-		if (i < n_T) {
-			idx = T_i[i];
-			fprintf(fp, "%20.10lf,%lf\n",
-					time[idx],
-					gyro_z[idx]);
-		} else {
-			fprintf(fp, ",,\n");
-		}
-	}
+	//close the file to release the lock
 	fclose(fp);
+	// delete this file so we don't process it again in the future
+	sprintf(delete_file, "rm %s", fname);
+	system(delete_file);
+
+	//free memory
+	if (line) 
+		free(line);
+	return 1;
+}
 
 
-	/* open the output file to write the stride data */
-	printf("Attempting to write to file \'%s\'.\n", ofile_st_name);
-	fp = fopen(ofile_st_name, "w");
-	if (fp == NULL) {
-		fprintf(stderr, 
-				"Failed to write to file \'%s\'.\n", 
-				ofile_st_name
-		       );
 
-		exit(EXIT_FAILURE);
+
+int main(int argc, char **argv)
+{	
+	FILE *fp;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	int fd;
+	char * fname;
+	char *ofile_feature_name
+
+	float pk_threshold;
+	pk_threshold = atof(argv[1]);
+	ofile_feature_name = argv[2];
+
+	while (1) {
+		system("ls -1 file_*.csv > fnames.txt");
+		// open the file that contains the file names that we need to inspect
+		fp = fopen("fnames.txt", "r");
+		if (fp == NULL) {
+			fprintf(stderr, "failed to open file\n");
+			exit(EXIT_FAILURE);
+		}
+		while ((read = getline(&line, &len, fp)) != -1) {
+			printf("Discovered file \'%s\'", line);
+			// strip newline from end of line read from file
+			fname = line;
+			fname[strlen(line)-1] = 0;
+			// process file with fname
+			printf("Processing file \'%s\'", fname);
+			process_file(fname,pk_threshold);
+
+			sleep(1);
+		}
+		// close the file that contains other filenames
+		fclose(fp);
+		// delete this file from the system
+		system("rm fnames.txt");
+		sleep(2);
 	}
-
-	fprintf(fp, "S_i,S_t,S_x\n");
-	for (i = 0; i < n_S; i++) {
-		idx = S_i[i];
-		fprintf(fp, "%d,%20.10lf,%lf\n",
-				idx,
-				time[idx],
-				gyro_z[idx]);
-	}
-	fclose(fp);
-	printf("stride_detector completed successfuly. Exiting.\n");
-
-	global_feature(accel_y, gyro_y, time, S_i, n_S, ofile_feature_name);
-	//Feature_file_generator
-	//exe_global_neural_network(ofile_feature_name);
-
-
-
-	return 0;
+	return EXIT_SUCCESS;
 }
